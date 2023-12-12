d4ae6b7aaba160aaa6ef08f8fbd87124f4b1ecf1
k1LoW <k1lowxb@gmail.com>
2023-12-12 17:04:40 +0900

    Add test

--- rfc9111/shared_test.go
+++ rfc9111/shared_test.go
@@ -7,6 +7,8 @@ import (
 )
 
 func TestShared_Storable(t *testing.T) {
+	now := time.Date(2024, 12, 13, 14, 15, 16, 00, time.UTC)
+
 	tests := []struct {
 		name        string
 		req         *http.Request
@@ -87,18 +89,164 @@ func TestShared_Storable(t *testing.T) {
 			time.Date(2024, 12, 13, 14, 15, 17, 00, time.UTC),
 		},
 		{
+			"GET 200 Last-Modified: 2024-12-13 14:15:06 -> +1s",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Last-Modified": []string{"Mon, 13 Dec 2024 14:15:06 GMT"},
+				},
+			},
+			true,
+			time.Date(2024, 12, 13, 14, 15, 17, 00, time.UTC),
+		},
+		{
+			"GET 500 Last-Modified: 2024-12-13 14:15:06 -> No Store",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusInternalServerError,
+				Header: http.Header{
+					"Last-Modified": []string{"Mon, 13 Dec 2024 14:15:06 GMT"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
 			"GET 200 -> No Store",
 			&http.Request{
 				Method: http.MethodGet,
 			},
 			&http.Response{
 				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Date": []string{"Mon, 13 Dec 2024 14:15:10 GMT"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
+			"UNUNDERSTOODMETHOD 200 -> No Store",
+			&http.Request{
+				Method: "UNUNDERSTOODMETHOD",
+			},
+			&http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Cache-Control": []string{"max-age=15"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
+			"GET 100 -> No Store",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusContinue,
+				Header: http.Header{
+					"Cache-Control": []string{"max-age=15"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
+			"GET 206 -> No Store",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusPartialContent,
+				Header: http.Header{
+					"Cache-Control": []string{"max-age=15"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
+			"GET 200 Cache-Control: no-store -> No Store",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Cache-Control": []string{"no-store"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
+			"GET 200 Cache-Control: private -> No Store",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Cache-Control": []string{"private"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
+			"GET 500 Cache-Control: public, Last-Modified 2024-12-13 14:15:06 -> +1s",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Last-Modified": []string{"Mon, 13 Dec 2024 14:15:06 GMT"},
+					"Cache-Control": []string{"public"},
+				},
+			},
+			true,
+			time.Date(2024, 12, 13, 14, 15, 17, 00, time.UTC),
+		},
+		{
+			"GET 500 Cache-Control: public -> No Store",
+			&http.Request{
+				Method: http.MethodGet,
+			},
+			&http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Cache-Control": []string{"public"},
+				},
+			},
+			false,
+			time.Time{},
+		},
+		{
+			"GET Authorization: XXX 200 Cache-Control: max-age=15 -> No Store",
+			&http.Request{
+				Method: http.MethodGet,
+				Header: http.Header{
+					"Authorization": []string{"XXX"},
+				},
+			},
+			&http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Cache-Control": []string{"max-age=15"},
+				},
 			},
 			false,
 			time.Time{},
 		},
 	}
-	now := time.Date(2024, 12, 13, 14, 15, 16, 00, time.UTC)
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
